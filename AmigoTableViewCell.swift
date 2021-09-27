66e4828527f2875c5647c2c9efe2cd64e3ab202f
juanjosecamachohidalgo <91477182+juanjosecamachohidalgo@users.noreply.github.com>
2021-09-27 15:41:44 +0200

    Initialize sesion04-def

---
+++ AmigoTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  AmigoTableViewCell.swift
+//  secondApp
+//
+//  Created by Inmaculada Mª Martínez Muñoz on 23/11/18.
+//  Copyright © 2018 Juan José Camacho Hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class AmigoTableViewCell: UITableViewCell {
+    
+    //MARK: propiedades
+    
+    @IBOutlet weak var foto: UIImageView!
+    
+    @IBOutlet weak var nombre: UILabel!
+    
+    @IBOutlet weak var evaluacion: EvaluacionControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AmigoTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  AmigoTableViewController.swift
+//  secondApp
+//
+//  Created by Inmaculada Mª Martínez Muñoz on 23/11/18.
+//  Copyright © 2018 Juan José Camacho Hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class AmigoTableViewController: UITableViewController {
+
+    var amigos = [Amigo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let amigo1 = Amigo(nombre: "Angelina", foto: UIImage(named:"Angelina")!, gAfinidad: 5)
+        let amigo2 = Amigo(nombre: "Esmeralda", foto: UIImage(named:"Esmeralda")!, gAfinidad: 5)
+        let amigo3 = Amigo(nombre: "David", foto: UIImage(named:"David")!, gAfinidad: 5)
+        
+        amigos += [amigo1!, amigo2!, amigo3!]
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return amigos.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AmigoTableViewCell", forIndexPath: indexPath) as! AmigoTableViewCell
+        cell.nombre.text = amigos[indexPath.row].nombre
+        cell.foto.image = amigos[indexPath.row].foto
+        cell.evaluacion.gradoAfinidad = amigos[indexPath.row].gradoAfinidad
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    //MARK: - Unwind segue desde AVC
+    @IBAction func addNuevoAmigo(sender: UIStoryboardSegue){
+        let sourceViewController = sender.sourceViewController as! AmigoViewController
+        let nuevoAmigo = sourceViewController.amigo
+        
+        amigos.append(nuevoAmigo!)
+        let newIndexPath = NSIndexPath(forRow: amigos.count-1, inSection: 0)
+        tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+        
+    }
+
+}
